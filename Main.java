bb3e666aacb1f1c176d37de83646ccccdde6ed3c
kjh0002 <wlsghks199@gmail.com>
2023-08-28 19:18:45 +0900

    #2023-08-28Git/2

--- Main.java
+++ Main.java
@@ -28,4 +28,10 @@ public class Main {
 
         System.out.println("result is " + result);
     }
+
+    private static int add(int number1, int number2) {
+        return number1+number2;
+    }
+
+    
 }
