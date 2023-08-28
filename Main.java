d3d127aab0046c3fd70cee365168791932a90cf8
minsu <zmfjtnl1@naver.com>
2023-08-28 18:26:30 +0900

    스켈레톤 작성

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * Main
+ */
+public class Main {
+
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a, b, c;
+        char operator;
+
+        System.out.println("Please enter a Expression");
+        a = scan.nextInt(); b = scan.nextInt(); c = scan.nextInt();
+        operator = scan.next().charAt(0);
+        switch(operator){
+            case '+':
+                c = add(a,b);
+                break;
+            case '-':
+                c = sub(a,b);
+            case '*':
+                c = multi(a, b);
+            case '/':
+                c = div(a, b);
+        }
+
+        System.out.println("result is " + c);
+    }
+}
